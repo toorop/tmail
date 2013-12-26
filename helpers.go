724726136a594f8516eab4f294b280e9d6afb680
Stéphane Depierrepont (Toorop) <toorop@toorop.fr>
2013-12-26 19:33:49 +0100

    Add nexUUid
    Signed-off-by: Stéphane Depierrepont (Toorop) <toorop@toorop.fr>

--- helpers.go
+++ helpers.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/rand"
+	"fmt"
+	"io"
 	"net"
 	"strings"
 )
@@ -63,4 +66,18 @@ func isStringInSlice(str string, s []string) (found bool) {
 		}
 	}
 	return
+}
+
+// generate uuid
+func newUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
 }
