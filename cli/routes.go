661fd98c45304a5ce461bd7d1ccdcccda07d82d3
Stéphane Depierrepont aka Toorop <toorop@toorop.fr>
2015-01-29 10:24:38 +0100

    empty destination to wildcard

--- cli/routes.go
+++ cli/routes.go
@@ -128,8 +128,13 @@ var Routes = cgCli.Command{
 				},
 			},
 			Action: func(c *cgCli.Context) {
+				// si la destination n'est pas renseignée on wildcard
+				host := c.String("d")
+				if host == "" {
+					host := "*"
+				}
 				// (host, localIp, remoteHost string, remotePort, priority int64, user, mailFrom, smtpAuthLogin, smtpAuthPasswd string)
-				err := api.RoutesAdd(c.String("d"), c.String("l"), c.String("rh"), c.Int("rp"), c.Int("p"), c.String("u"), c.String("f"), c.String("rl"), c.String("rpwd"))
+				err := api.RoutesAdd(host), c.String("l"), c.String("rh"), c.Int("rp"), c.Int("p"), c.String("u"), c.String("f"), c.String("rl"), c.String("rpwd"))
 				cliHandleErr(err)
 			},
 		},
