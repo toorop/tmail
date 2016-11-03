48839a7e92cb52fdf72fb03c0a41b5a3ce4b79de
Stéphane Depierrepont aka Toorop <toorop@toorop.fr>
2016-11-03 16:48:44 +0100

    add postinit plugin hook

--- core/plugin.go
+++ core/plugin.go
@@ -1,5 +1,12 @@
 package core
 
+// TmailPlugin base plugin for hooks:
+// - postinit
+type TmailPlugin func() error
+
+// TmailPlugins is a map of plugin
+var TmailPlugins map[string][]TmailPlugin
+
 // SMTPdPlugin is the type for SMTPd plugins
 type SMTPdPlugin func(s *SMTPServerSession) bool
 
@@ -7,11 +14,26 @@ type SMTPdPlugin func(s *SMTPServerSession) bool
 var SMTPdPlugins map[string][]SMTPdPlugin
 
 func init() {
+	TmailPlugins = make(map[string][]TmailPlugin)
 	SMTPdPlugins = make(map[string][]SMTPdPlugin)
 }
 
-// RegisterSMTPdPlugin registers a new plugin
+// RegisterPlugin registers a new plugin
+func RegisterPlugin(hook string, plugin TmailPlugin) {
+	TmailPlugins[hook] = append(TmailPlugins[hook], plugin)
+}
+
+// RegisterSMTPdPlugin registers a new smtpd plugin
 // TODO check hook
 func RegisterSMTPdPlugin(hook string, plugin SMTPdPlugin) {
 	SMTPdPlugins[hook] = append(SMTPdPlugins[hook], plugin)
+}
+
+func execTmailPlugins(hook string) {
+	if plugins, found := TmailPlugins[hook]; found {
+		for _, plugin := range plugins {
+			plugin()
+		}
+	}
+	return
 }
--- plugin_import.go
+++ plugin_import.go
@@ -1,7 +1,9 @@
 package main
 
-import _ "github.com/toorop/protecmail/tmailplugins/connect"
+//import _ "github.com/toorop/protecmail/tmailplugins/connect"
 
 //import _ "github.com/toorop/protecmail/tmailplugins/helo"
 
-import _ "github.com/toorop/protecmail/tmailplugins/mail"
+//import _ "github.com/toorop/protecmail/tmailplugins/mail"
+
+import _ "github.com/toorop/protecmail/tmailplugins/postinit"
